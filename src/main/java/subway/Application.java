7817d5e7ce94858e7640d7031119b2aeb1e0251a
julie <7jujubuilds@gmail.com>
2024-12-12 14:50:14 +0900

    feat(input): 계산 선택지 입력 기능 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,16 @@
 package subway;
 
-import java.util.Scanner;
+import subway.domain.Handler;
+import subway.helpers.InputController;
+import subway.ui.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        InputController inputController = new InputController(new InputView());
+
+        String option = inputController.getOption();
+        if (option.equals("Q")) {
+            return;
+        }
     }
 }
--- src/main/java/subway/helpers/InputController.java
+++ src/main/java/subway/helpers/InputController.java
@@ -11,21 +11,49 @@ public class InputController {
         this.inputView = inputView;
     }
 
-    public String getMenuSelection() {
-        System.out.println(UiText.MAIN_MENU_PROMPT.getText());
+    public String getOption() {
+        String response = "";
         while (true) {
             try {
-                String response = inputView.readOption();
-                this.validateOption(response);
-                return response;
+                response = this.getFirstMenu();
+                if (response.equals("Q")) {
+                    break;
+                }
+                response = this.getSecondMenu();
+                if (response.equals("B")) {
+                    continue;
+                }
+                break;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
+
+        }
+        return response;
+    }
+
+    public String getFirstMenu() {
+        System.out.println(UiText.MAIN_MENU_PROMPT.getText());
+        String response = inputView.readOption();
+        this.validateFirstOption(response);
+        return response;
+    }
+
+    public String getSecondMenu() {
+        System.out.println(UiText.OPTIONS_MENU_PROMPT.getText());
+        String response = inputView.readOption();
+        this.validateSecondOption(response);
+        return response;
+    }
+
+    private void validateFirstOption(String option) {
+        if (!option.equals("1") && !option.equals("Q")) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID.getMessage());
         }
     }
 
-    private void validateOption(String option) {
-        if (!option.equals("1") && !option.equals("Q")) {
+    private void validateSecondOption(String option) {
+        if (!option.equals("1") && !option.equals("2") && !option.equals("B")) {
             throw new IllegalArgumentException(ErrorMessages.INVALID.getMessage());
         }
     }
